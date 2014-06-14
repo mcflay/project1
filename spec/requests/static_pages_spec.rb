4f632a46dc1a7460e4990e29e2d8317d1d3cc6da
Marcin <mcflay19@gmail.com>
2014-06-14 20:46:25 +0200

    added 'paths' and workings links'

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,26 +3,32 @@ require 'spec_helper'
 describe "StaticPages" do
   describe "home page" do
   	it "should have the content 'home'" do
-  		visit '/static_pages/home'
+  		visit root_path
   		expect(page).to have_content('Sample App')
   	end	
   end
   describe "about page" do
   	it "should have the content 'about'" do
-  		visit '/static_pages/about'
+  		visit about_path
   		expect(page).to have_content('About')
   	end
   end
   describe "help page" do
   	it "Should have the content 'help'" do
-  		visit '/static_pages/help'
+  		visit help_path
   		expect(page).to have_content('Help')
   	end
   end
   describe "contact page" do
   	it "Should have the content 'Contact'" do
-  		visit '/static_pages/contact'
+  		visit contact_path
   		expect(page).to have_content('Contact')
   	end
+  end
+  describe "projects page" do
+    it "should have the content 'Project'" do
+      visit projects_path
+      expect(page).to have_content('Projects')
+    end
   end
 end
